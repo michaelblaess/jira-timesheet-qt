29fea038053f0bdcc4004749b44a6a66a5bb6da4
Michael Blaess <mail@michaelblaess.de>
2026-07-30 18:01:29 +0200

    release: v0.3.0

--- src/jira_timesheet_qt/__init__.py
+++ src/jira_timesheet_qt/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __author__ = "Michael Blaess"
 __year__ = "2026"
 # Anzeigename der Anwendung. Bewusst der Werkzeugname und NICHT das deutsche
